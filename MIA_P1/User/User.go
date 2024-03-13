ff8fff07dbd1323252df04c2892287f35199722a
Sergie Arizandieta <78749398+SergieArizandieta@users.noreply.github.com>
2024-03-13 15:51:29 -0600

    [ADD] search inodes func

--- MIA_P1/User/User.go
+++ MIA_P1/User/User.go
@@ -2,15 +2,16 @@ package User
 
 import (
 	//  "os"
-    "fmt"
-    "strings"
-    "encoding/binary"
-    "MIA_P1/Utilities"
-    "MIA_P1/Structs"
+	"MIA_P1/Structs"
+	"MIA_P1/Utilities"
+	"MIA_P1/UtilitiesInodes"
+	"encoding/binary"
+	"fmt"
+	"strings"
 )
 
-//login -user=root -pass=123 -id=A119
-func Login(user string, pass string, id string){
+// login -user=root -pass=123 -id=A119
+func Login(user string, pass string, id string) {
 	fmt.Println("======Start LOGIN======")
 	fmt.Println("User:", user)
 	fmt.Println("Pass:", pass)
@@ -18,14 +19,14 @@ func Login(user string, pass string, id string){
 
 	driveletter := string(id[0])
 
-   // Open bin file
-	filepath := "./test/" + strings.ToUpper(driveletter)  + ".bin"
+	// Open bin file
+	filepath := "./test/" + strings.ToUpper(driveletter) + ".bin"
 	file, err := Utilities.OpenFile(filepath)
 	if err != nil {
 		return
 	}
 
-   var TempMBR Structs.MRB
+	var TempMBR Structs.MRB
 	// Read object from bin file
 	if err := Utilities.ReadObject(file, &TempMBR, 0); err != nil {
 		return
@@ -36,30 +37,30 @@ func Login(user string, pass string, id string){
 
 	fmt.Println("-------------")
 
-   var index int = -1
-   // Iterate over the partitions
-   for i := 0; i < 4; i++ {
-      if TempMBR.Partitions[i].Size != 0 {
-         if strings.Contains(string(TempMBR.Partitions[i].Id[:]), id) {
-            fmt.Println("Partition found")
-            if strings.Contains(string(TempMBR.Partitions[i].Status[:]), "1") {
-               fmt.Println("Partition is mounted")
-               index = i
-            }else{
-               fmt.Println("Partition is not mounted")
-               return
-            }
-            break
-         }
-      }
-   }
+	var index int = -1
+	// Iterate over the partitions
+	for i := 0; i < 4; i++ {
+		if TempMBR.Partitions[i].Size != 0 {
+			if strings.Contains(string(TempMBR.Partitions[i].Id[:]), id) {
+				fmt.Println("Partition found")
+				if strings.Contains(string(TempMBR.Partitions[i].Status[:]), "1") {
+					fmt.Println("Partition is mounted")
+					index = i
+				} else {
+					fmt.Println("Partition is not mounted")
+					return
+				}
+				break
+			}
+		}
+	}
 
-   if index != -1 {
-      Structs.PrintPartition(TempMBR.Partitions[index])
-   }else{
-      fmt.Println("Partition not found")
-      return
-   }
+	if index != -1 {
+		Structs.PrintPartition(TempMBR.Partitions[index])
+	} else {
+		fmt.Println("Partition not found")
+		return
+	}
 
 	var tempSuperblock Structs.Superblock
 	// Read object from bin file
@@ -69,20 +70,21 @@ func Login(user string, pass string, id string){
 
 	// initSearch /users.txt -> regresa no Inodo
 	// initSearch -> 1
+	indexInode := UtilitiesInodes.InitSearch("/users.txt", file, tempSuperblock)
 
-	indexInode := int32(1);
+	// indexInode := int32(1)
 
 	var crrInode Structs.Inode
 	// Read object from bin file
-	if err := Utilities.ReadObject(file, &crrInode, int64(tempSuperblock.S_inode_start + indexInode * int32(binary.Size(Structs.Inode{})))); err != nil {
+	if err := Utilities.ReadObject(file, &crrInode, int64(tempSuperblock.S_inode_start+indexInode*int32(binary.Size(Structs.Inode{})))); err != nil {
 		return
 	}
 
 	// getInodeFileData -> Iterate the I_Block n concat the data
-	
-	var Fileblock Structs.Fileblock 
+
+	var Fileblock Structs.Fileblock
 	// Read object from bin file
-	if err := Utilities.ReadObject(file, &Fileblock, int64(tempSuperblock.S_block_start + crrInode.I_block[0] * int32(binary.Size(Structs.Fileblock{})))); err != nil {
+	if err := Utilities.ReadObject(file, &Fileblock, int64(tempSuperblock.S_block_start+crrInode.I_block[0]*int32(binary.Size(Structs.Fileblock{})))); err != nil {
 		return
 	}
 
@@ -94,10 +96,9 @@ func Login(user string, pass string, id string){
 	// Iterar a través de las líneas
 	for _, line := range lines {
 		// Imprimir cada línea
-		fmt.Println(line)		
-		
+		fmt.Println(line)
+
 	}
-	
 
 	// Print object
 	fmt.Println("Inode", crrInode.I_block)
@@ -105,6 +106,5 @@ func Login(user string, pass string, id string){
 	// Close bin file
 	defer file.Close()
 
-
-	fmt.Println("======End LOGIN======")	
+	fmt.Println("======End LOGIN======")
 }
---
+++ MIA_P1/UtilitiesInodes/UtilitiesInodes.go
@@ -0,0 +1,97 @@
+package UtilitiesInodes
+
+import (
+	"MIA_P1/Structs"
+	"MIA_P1/Utilities"
+	"encoding/binary"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// login -user=root -pass=123 -id=A119
+func InitSearch(path string, file *os.File, tempSuperblock Structs.Superblock) int32 {
+	fmt.Println("======Start INITSEARCH======")
+	fmt.Println("path:", path)
+	// path = "/ruta/nueva"
+
+	// split the path by /
+	TempStepsPath := strings.Split(path, "/")
+	StepsPath := TempStepsPath[1:]
+
+	fmt.Println("StepsPath:", StepsPath, "len(StepsPath):", len(StepsPath))
+	for _, step := range StepsPath {
+		fmt.Println("step:", step)
+	}
+
+	var Inode0 Structs.Inode
+	// Read object from bin file
+	if err := Utilities.ReadObject(file, &Inode0, int64(tempSuperblock.S_inode_start)); err != nil {
+		return -1
+	}
+
+	fmt.Println("======End INITSEARCH======")
+
+	return SarchInodeByPath(StepsPath, Inode0, file, tempSuperblock)
+}
+
+func pop(s *[]string) string {
+	lastIndex := len(*s) - 1
+	last := (*s)[lastIndex]
+	*s = (*s)[:lastIndex]
+	return last
+}
+
+// login -user=root -pass=123 -id=A119
+func SarchInodeByPath(StepsPath []string, Inode Structs.Inode, file *os.File, tempSuperblock Structs.Superblock) int32 {
+	fmt.Println("======Start SARCHINODEBYPATH======")
+	index := int32(0)
+	SearchedName := strings.Replace(pop(&StepsPath), " ", "", -1)
+
+	fmt.Println("========== SearchedName:", SearchedName)
+
+	// Iterate over i_blocks from Inode
+	for _, block := range Inode.I_block {
+		if block != -1 {
+			if index < 13 {
+				//CASO DIRECTO
+
+				var crrFolderBlock Structs.Folderblock
+				// Read object from bin file
+				if err := Utilities.ReadObject(file, &crrFolderBlock, int64(tempSuperblock.S_block_start+block*int32(binary.Size(Structs.Inode{})))); err != nil {
+					return -1
+				}
+
+				for _, folder := range crrFolderBlock.B_content {
+					// fmt.Println("Folder found======")
+					fmt.Println("Folder === Name:", string(folder.B_name[:]), "B_inodo", folder.B_inodo)
+
+					if strings.Contains(string(folder.B_name[:]), SearchedName) {
+
+						fmt.Println("len(StepsPath)", len(StepsPath), "StepsPath", StepsPath)
+						if len(StepsPath) == 0 {
+							fmt.Println("Folder found======")
+							return folder.B_inodo
+						} else {
+							fmt.Println("NextInode======")
+							var NextInode Structs.Inode
+							// Read object from bin file
+							if err := Utilities.ReadObject(file, &NextInode, int64(tempSuperblock.S_inode_start+folder.B_inodo*int32(binary.Size(Structs.Inode{})))); err != nil {
+								return -1
+							}
+							return SarchInodeByPath(StepsPath, NextInode, file, tempSuperblock)
+						}
+					}
+				}
+
+			} else {
+				//CASO INDIRECTO
+			}
+		}
+		index++
+	}
+
+	fmt.Println("======End SARCHINODEBYPATH======")
+
+	return 0
+}
